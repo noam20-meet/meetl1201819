643b54fba8ed1f1acbbc723f924e58d53553b9fd
maayan20-meet <maayan20@meet.mit.edu>
2018-11-11 12:28:28 -0500

    lab5

--- lab5.py
+++ lab5.py
@@ -37,17 +37,79 @@
 # and has a method signature of "speak" which
 # prints the object as "Jefferson, Thomas".
 
-class Person(object):
-  def __init__(self, first_name, last_name):
-    self.first = first_name
-    self.last = last_name
-  def speak(self):
-  	print("My name is " + self.first + " " + self.last)
+#class Person(object):
+  #def __init__(self, first_name, last_name):
+    #self.first = first_name
+    #self.last = last_name
+  #def speak(self):
+  	#print("My name is " + self.first + " " + self.last)
 
-me = Person("Brandon", "Walsh")
-you = Person("Ethan", "Reed")
+#me = Person("Brandon", "Walsh")
+#you = Person("Ethan", "Reed")
 
-me.speak()
-you.speak()
+#me.speak()
+#you.speak()
+
+
+
+import tkinter as tk
+from tkinter import simpledialog
+# Calculating Grades (ok, let me think about this one)
+
+# Write a program that will average 3 numeric exam grades, return an average test score, a corresponding letter grade, and a message stating whether the student is passing.
+
+# Average    Grade
+# 90+    A
+# 80-89    B
+# 70-79    C
+# 60-69    D
+# 0-59    F
+
+# Exams: 89, 90, 90
+# Average: 90
+# Grade: A
+# Student is passing.
+
+# Exams: 50, 51, 0
+# Average: 33
+# Grade: F
+# Student is failing.
+
+exam_one = int(simpledialog.askstring("Input", "input exam grade one", parent=tk.Tk().withdraw()))
+
+exam_two = int(simpledialog.askstring("Input exam grade two: ", parent=tk.Tk().withdraw()))
+
+exam_3 = int(simpledialog.askstring("Input exam grade three: ", parent=tk.Tk().withdraw()))
+
+grades = [exam_one exam_two exam_three]
+sum = 0
+for grade in grade:
+  sum = sum + grade
+
+avg = sum / len(grdes)
+
+if avg >= 90:
+    letter_grade = "A"
+elif avg >= 80 and avg < 90:
+    letter_grade = "B"
+elif avg > 69 and avg < 80:
+    letter_grade = "C"
+elif avg <= 69 and avg >= 65:
+    letter_grade = "D"
+else:
+    letter_grade = "F"
+
+for grade in grades:
+    print("Exam: " + str(grade))
+
+    print("Average: " + str(avg))
+
+    print("Grade: " + letter_grade)
+
+if letter_grade is == "F":
+    print("Student is failing.")
+else:
+    print ("Student is passing.")
+
 
 
