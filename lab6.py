993db1963adc74b6a5a05438c7d00a7de29a97ae
maayan20-meet <maayan20@meet.mit.edu>
2018-12-02 13:02:49 -0500

    lab6

--- lab6.py
+++ lab6.py
@@ -1,3 +1,4 @@
+''''
 from turtle import Turtle
 import turtle
 import random
@@ -10,19 +11,38 @@ class Square(Turtle):
 	def random_color(self):
 		colors= ["yellow", "pink", "blue", "green"]
 		c= random.randint(0,3)
-		self.color(color[c])
+		self.color(colors[c])
 
 s= Square(8)
 
 s.random_color()
 
 turtle.mainloop()
+'''
+import turtle
+from turtle import Turtle
+
+class Hexagon (Turtle):
+	def __init__(self, size):
+		Turtle.__init__(self) 
+		self.size=size
+		turtle.penup()
+	
+		turtle.begin_poly()
+		for i  in range(6):
+			turtle.forward(100)
+			turtle.right(60)
+		turtle.end_poly()
+		s= turtle.get_poly()
+		turtle.register_shape("hexagon", s)
+		self.shape("hexagon")
+		
+
+t1=Hexagon(50)
 
 
 
 
 
 
-
-
-
+turtle.mainloop()
